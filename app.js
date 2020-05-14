f813320962f9e526ddb04ea6caaaba6ff686599e
Chitranshi Saxena <csaxena@kloudrac.com>
2020-05-14 15:20:53 +0530

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayouts = require('express-ejs-layouts');
 var dotenv = require('dotenv');
 dotenv.config();
 var indexRouter = require('./routes/index');
@@ -10,19 +11,27 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+//EJS
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
+
+
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
+const PORT =process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server started on port ${PORT}'))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,47 @@ const pool = require('../db/dbConfig');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hii this is inside user.js file');
+  res.send('respond with a resource');
 });
 
 router.get('/contactList',(request, response) => {
 
   pool
-  .query('SELECT sfid, name FROM salesforce.Contact')
+  .query('SELECT sfid, name, Email FROM salesforce.Contact')
   .then((contactQueryResult) => {
   console.log('contactQueryResult : '+JSON.stringify(contactQueryResult.rows));
   response.send(contactQueryResult.rows);
   })
-  
   .catch((contactQueryError)=>{
   response.send(contactQueryError);
   })
   
   });
 
-module.exports = router;
+  router.post('/',(request, response) => {
+    let body = request.body;
+    console.log('body '+JSON.stringify(body));
+  
+  
+})
+router.get('/login',(request, response) => {
+  response.render('login.ejs');
+})
+
+
+router.post('/loginPost',(request, response) => {
+  let body = request.body;
+  console.log('Your Response body is :    '+JSON.stringify(body));
+
+  response.send('Hello  '+body.email);
+})
+
+  //Register Page
+  router.get('/register', function(req, res) {
+    res.render('register.ejs');
+  });
+ 
+  router.get('/list',(request, response) => {
+    response.render('contactInsert.ejs');
+  })
+module.exports = router
